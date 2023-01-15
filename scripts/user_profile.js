525dbf76e3964214490a8ee13fb6f137cca12d37
valon haxhidauti <valonidv10@hotmail.com>
2023-01-15 23:08:14 +0100

    fetching api to show photos

--- scripts/user_profile.js
+++ scripts/user_profile.js
@@ -2,54 +2,26 @@
 
 
 
-// const a = fetch('https://api.npoint.io/edaa98bc6b3b2a0f645e')
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
-
-// fetch("https://api.npoint.io/edaa98bc6b3b2a0f645e")
-//     .then(response => response.json())
-//     .then(data => {
-//         console.log(data.data.user.edge_owner_to_timeline_media.edges[0].node.display_url);
-//     })
-//     .catch(error => console.error(error));
-
-// // console.log (a)
-// console.log (b)
-
-// async function getData(index){
-//     try{
-//         let res = await fetch("https://api.npoint.io/edaa98bc6b3b2a0f645e");
-//         let data = await res.json();
-//         let photo = data.data.user.edge_owner_to_timeline_media.edges[index].node.display_url
-//         return photo
-//     }catch(err){
-//         console.error(err);
-//     }
-//  }
-
-// const a = getData(0)
-// console.log(a)
-// let data;
 async function getData(){
    try{
        let res = await fetch("https://api.npoint.io/edaa98bc6b3b2a0f645e");
        let data = await res.json();
-       const photos = [];
-       for (let i = 0; i < 10; i++) {
-        photos.push(data.data.user.edge_owner_to_timeline_media.edges[i].node.display_url);
-       }
-       return photos;
+        return data
    }catch(err){
        console.error(err);
    }
 }
 
-// const a = getData().then(res => console.log(res))
-// var results;
-// getData().then(res => results = res)
 
-//  console.log(results)
 
-// const feed1 = document.querySelector(".feed1");
-// feed1.style.backgroundImage = `url("${results}")`
+getData().then(res => {showPosts(res)})
+
+// CORS error --- net::ERR_BLOCKED_BY_RESPONSE.NotSameOrigin 200
+const showPosts = (allPosts) => {
+    let feeds = document.querySelectorAll(`.feed`);
+    for (let i = 0; i < 3; i++) {
+        const photo = allPosts.data.user.edge_owner_to_timeline_media.edges[i].node.display_url
+        feeds[i].style.backgroundImage = `url("${photo}")`
+    }
+
+}
