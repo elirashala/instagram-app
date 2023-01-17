2529db0711620740d4c4063e0b10198a3db7ccda
Trim Kusari <trim.kusari@life.gjirafa.com>
2023-01-17 15:56:16 +0100

    Navigation Bar Functionality

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -55,30 +55,76 @@
 
     changeIcon(tabletWindow);
 
-    // Search
-    const searchButton = document.querySelector('#search-button');
-    const searchBar = document.querySelector('.search');
+
+    // Nav Bar Selection
     const navBar = document.querySelector('.nav-bar');
     const navMenu = document.querySelectorAll('.menu');
 
-    searchButton.addEventListener('click', () => {
-        searchButton.classList.toggle('active');
-        searchBar.classList.toggle('active');
+    // Menu Buttons
+    const menuButtons = document.querySelectorAll('.menu-button');
+    
+    // Add an event listener to each button
+    menuButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            // check which button is active
+            if(this.classList.contains('active')){
+                this.classList.remove('active');
+                
+            }else{
+            // Remove the active class from all menuButtons
+            menuButtons.forEach(b => b.classList.remove('active'));
+            searchBar.classList.remove('active');
+            notificationBar.classList.remove('active');
+            navBar.classList.remove('transition');
+            for (const element of navMenu) {
+                element.classList.remove('transition');
+            }
+            instaLogo1.style.display = "block";
+            instaLogo2.style.display = "none";  
 
+            tabletWindow.addListener(changeIcon);
+            changeIcon(tabletWindow);
 
-        // instaLogo1.style.display = "none";
-        // instaLogo2.style.display = "block";
+            // Add the active class to the clicked button
+            this.classList.add('active');
+            console.log('active');
+            }
+        });
+    });
+
+    // Logo Display for Tablet Media Query
+    function tablet(media){
+        if(media.matches){
+            instaLogo1.style.display = "none";
+            instaLogo2.style.display = "block";  
+        }
+    }
+
+    // Search
+    const searchButton = document.querySelector('#search-button');
+    const searchBar = document.querySelector('.search');
+    
+    searchButton.addEventListener('click', function(){
+        // this.classList.toggle('active');
+
+        if(this.classList.contains('active')){
+            searchBar.classList.toggle('active');
+        }else{
+            searchBar.classList.remove('active');    
+        }
 
         if (instaLogo1.style.display === "block") {
             instaLogo1.style.display = "none";
-            instaLogo2.style.display = "block";
+            instaLogo2.style.display = "block";   
         }
         else {
             instaLogo1.style.display = "block";
             instaLogo2.style.display = "none";
         }
 
-        // navbar.style.cssText = 'width: 95px; transition: 0.7s;';
+        tabletWindow.addListener(tablet);
+        tablet(tabletWindow);
+
         navBar.classList.toggle('transition');
         for (const element of navMenu) {
             element.classList.toggle('transition');
@@ -86,11 +132,42 @@
 
     })
 
+    // Notification
+    const notificationButton = document.querySelector('#notification-button');
+    const notificationBar = document.querySelector('.notification');
+
+    notificationButton.addEventListener('click', function(){
+        // this.classList.toggle('active');
+
+        if(this.classList.contains('active')){
+            notificationBar.classList.toggle('active');
+        }else{
+            notificationBar.classList.remove('active');    
+        }
+
+        if (instaLogo1.style.display === "block") {
+            instaLogo1.style.display = "none";
+            instaLogo2.style.display = "block";   
+        }
+        else {
+            instaLogo1.style.display = "block";
+            instaLogo2.style.display = "none";
+        }
+
+        tabletWindow.addListener(tablet);
+        tablet(tabletWindow);
+
+        navBar.classList.toggle('transition');
+        for (const element of navMenu) {
+            element.classList.toggle('transition');
+        }
+    })
+
     // Clear Input Value
     const searchIcon = document.querySelector('.search-icon');
     const searchInput = document.querySelector('#search-input');
     const clearInput = document.querySelector('.clear-input');
-
+    
     searchInput.addEventListener('click', () => {
         // console.log('input is clicked');
         if (clearInput.style.display === "none"){
@@ -105,40 +182,3 @@
         clearInput.style.display = "none";
         searchIcon.style.display = "block";
     })
-
-
-    // Notification
-    const notificationButton = document.querySelector('#notification-button');
-    const notificationBar = document.querySelector('.notification');
-
-    notificationButton.addEventListener('click', () => {
-        notificationButton.classList.toggle('active');
-        notificationBar.classList.toggle('active');
-
-        if (instaLogo1.style.display === "block") {
-            instaLogo1.style.display = "none";
-            instaLogo2.style.display = "block";
-        }
-        else {
-            instaLogo1.style.display = "block";
-            instaLogo2.style.display = "none";
-        }
-
-        navBar.classList.toggle('transition');
-        for (const element of navMenu) {
-            element.classList.toggle('transition');
-        }
-
-    })
-
-    // Mi rishiku perseri
-    // const menuButtons = document.querySelectorAll('.menu-button');
-
-    // menuButtons.forEach(button => {
-    //     button.addEventListener('click', function() {
-    //     menuButtons.forEach(button => {
-    //         button.classList.remove('active');
-    //     });
-    //     this.classList.add('active');
-    //     });
-    // });
