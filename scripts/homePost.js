7a5a1783950ace9500d3663bb3f0206f8f4ba22f
Trim Kusari <trim.kusari@life.gjirafa.com>
2023-01-24 15:14:22 +0100

    A simple design for the post content
    A simple design for the post content of what it might look like. To start getting the data for this content*

---
+++ scripts/homePost.js
@@ -0,0 +1,30 @@
+
+let homePost =
+    `
+    <div class="post">
+        <div class="post-user">
+            <img src="" alt="">
+            <h4>username</h4>
+        </div>
+        <div class="post-image">
+            <img src="" alt="">
+        </div>
+        <div class="post-container">
+            <div class="post-lmsh">
+                <svg id="" class="" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 -4.5 32 38">
+                    <path
+                    d="M13.83,18.08l.16.18-.17-.17Z"/><path d="M29,18.39C26.25,21,23.37,23.45,20.54,26c-1.22,1.09-2.45,2.16-3.67,3.23l-.46.39a1.39,1.39,0,0,1-2-.07c-.95-.9-1.88-1.84-2.83-2.76C8.79,24.07,6,21.4,3.27,18.7A10.37,10.37,0,0,1,.21,13.43C-.77,8.82,1.79,4,6.05,2.52A8.47,8.47,0,0,1,15.61,5.1l.29.34a24.65,24.65,0,0,1,2.25-1.92,8.78,8.78,0,0,1,13.35,5A9.19,9.19,0,0,1,29,18.39Z"/>
+                </svg>
+            </div>
+            <div class="post-likes">
+                <p>100000 likes</p>
+            </div>
+            <div class="post-dc">
+                <p>USER - description</p>
+            </div>
+            <div class="post-comments">
+                <p>View all 100 comments</p>
+            </div>
+        </div>
+    </div>
+`;
