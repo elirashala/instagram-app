0691b8eece2a866a0c6e51b7163906a7eb8830f1
Trim Kusari <trim.kusari@life.gjirafa.com>
2023-01-25 18:23:53 +0100

    Get posts in home feed

--- scripts/homePost.js
+++ scripts/homePost.js
@@ -1,30 +1,185 @@
+// const postFeed = document.querySelector('.posts-content');
+// const userId = `https://api.npoint.io/ad27d2a267a4669527c8`;
 
-let homePost =
-    `
-    <div class="post">
-        <div class="post-user">
-            <img src="" alt="">
-            <h4>username</h4>
-        </div>
-        <div class="post-image">
-            <img src="" alt="">
-        </div>
-        <div class="post-container">
-            <div class="post-lmsh">
-                <svg id="" class="" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 -4.5 32 38">
-                    <path
-                    d="M13.83,18.08l.16.18-.17-.17Z"/><path d="M29,18.39C26.25,21,23.37,23.45,20.54,26c-1.22,1.09-2.45,2.16-3.67,3.23l-.46.39a1.39,1.39,0,0,1-2-.07c-.95-.9-1.88-1.84-2.83-2.76C8.79,24.07,6,21.4,3.27,18.7A10.37,10.37,0,0,1,.21,13.43C-.77,8.82,1.79,4,6.05,2.52A8.47,8.47,0,0,1,15.61,5.1l.29.34a24.65,24.65,0,0,1,2.25-1.92,8.78,8.78,0,0,1,13.35,5A9.19,9.19,0,0,1,29,18.39Z"/>
+// const getPosts = async () => {
+//     try{
+//     // const userIdRes = await fetch(userId);
+//     // const userIdData = await userIdRes.json();
+
+//     // const userInfo = `https://api.npoint.io/9547d5a7739c9e283f62?pk=${userIdData.id}`;
+
+    
+
+//     // const userInfoRes = await fetch(userInfo);
+//     // const userInfoData = await userInfoRes.json();
+
+//     const userPosts = `https://api.npoint.io/b743418638800ff41502`;
+//     const postsRes = await fetch(userPosts);
+//     const postsData = await postsRes.json();
+
+//     // console.log(userIdData);
+//     // console.log(userInfoData);
+//     console.log(postsData);
+
+    
+//     return postsData;
+    
+//     }catch(error){
+//         console.error(error);
+//     }
+// }
+
+// const showPosts = (element) => {
+//     for(let i = 0; i < 8; i++){     
+//             postFeed.innerHTML +=
+//             `
+//             <div class="post">
+//                 <div class="post-user">
+//                     <img src="https://source.unsplash.com/random/200x200?sig=imgId" alt="">
+//                     <h4></h4>
+//                 </div>
+//                 <div class="post-image">
+//                     <img src="https://source.unsplash.com/random/300x400?sig=imgId" alt="">
+//                 </div>
+//                 <div class="post-container">
+//                     <div class="post-lmsh">
+//                         <svg id="" class="" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 -4.5 32 38">
+//                             <path
+//                             d="M13.83,18.08l.16.18-.17-.17Z"/><path d="M29,18.39C26.25,21,23.37,23.45,20.54,26c-1.22,1.09-2.45,2.16-3.67,3.23l-.46.39a1.39,1.39,0,0,1-2-.07c-.95-.9-1.88-1.84-2.83-2.76C8.79,24.07,6,21.4,3.27,18.7A10.37,10.37,0,0,1,.21,13.43C-.77,8.82,1.79,4,6.05,2.52A8.47,8.47,0,0,1,15.61,5.1l.29.34a24.65,24.65,0,0,1,2.25-1.92,8.78,8.78,0,0,1,13.35,5A9.19,9.19,0,0,1,29,18.39Z"/>
+//                         </svg>
+//                     </div>
+//                     <div class="post-likes">
+//                         <p>${element.data.user.edge_owner_to_timeline_media.edges[i].node.edge_media_preview_like.count} 1 00000 likes</p>
+//                     </div>
+//                     <div class="post-dc">
+//                         <p>USER - description</p>
+//                     </div>
+//                     <div class="post-comments">
+//                         <p>View all 100 comments</p>
+//                     </div>
+//                 </div>
+//             </div>
+//         `;
+//     };
+// }
+
+
+
+
+// getPosts()
+// .then(res => {showPosts(res)});
+
+
+const postFeed = document.querySelector('.posts-content');
+let userId;
+
+const getUserId = async () => {
+    try {
+        const userIdRes = await fetch(`https://api.npoint.io/ad27d2a267a4669527c8`);
+        const userIdData = await userIdRes.json();
+        userId = userIdData.id;
+        console.log(userIdData);
+    }catch(error){
+        console.error(error);
+    }
+}
+
+const getPosts = async () => {
+    try{
+        const userInfoRes = await fetch(`https://api.npoint.io/9547d5a7739c9e283f62?pk=${userId}`);
+        const userInfoData = await userInfoRes.json();
+
+        const userPosts = `https://api.npoint.io/b743418638800ff41502?id=${userInfoData.pk}`;
+        const postsRes = await fetch(userPosts);
+        const postsData = await postsRes.json();
+
+        console.log(userInfoData);
+        console.log(postsData);
+
+        return {userInfo: userInfoData, posts: postsData};
+    }catch(error){
+        console.log(error);
+    }
+}
+
+
+const showPosts = (datas) => {
+    const {userInfo, posts} = datas;
+    
+    for(let i = 0; i <userInfo.user.media_count; i++){
+        postFeed.innerHTML +=
+        `
+        <div class="post">
+            <div class="post-user">
+                <img src="https://source.unsplash.com/random/200x200?sig=imgId" alt="">
+                <h4>${userInfo.user.username}</h4>
+            </div>
+            <div class="post-image">
+                <img src="https://source.unsplash.com/random/300x400?sig=imgId" alt="">
+            </div>
+            <div class="post-container">
+                <div class="post-lmsh">
+                <svg xmlns="http://www.w3.org/2000/svg" class="no-like" width="36" height="36" viewBox="0 0 24 24" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
+                    <path 
+                    d="M12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412l7.332 7.332c.17.299.498.492.875.492a.99.99 0 0 0 .792-.409l7.415-7.415c2.354-2.354 2.354-6.049-.002-8.416a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595zm6.791 1.61c1.563 1.571 1.564 4.025.002 5.588L12 18.586l-6.793-6.793c-1.562-1.563-1.561-4.017-.002-5.584.76-.756 1.754-1.172 2.799-1.172s2.035.416 2.789 1.17l.5.5a.999.999 0 0 0 1.414 0l.5-.5c1.512-1.509 4.074-1.505 5.584-.002z">
+                    </path>
                 </svg>
-            </div>
-            <div class="post-likes">
-                <p>100000 likes</p>
-            </div>
-            <div class="post-dc">
-                <p>USER - description</p>
-            </div>
-            <div class="post-comments">
-                <p>View all 100 comments</p>
+                <svg xmlns="http://www.w3.org/2000/svg" class="like-heart" style="display:none; fill: rgba(255, 67, 67, 1)" width="36" height="36" viewBox="0 0 24 24">
+                    <path 
+                    d="M20.205 4.791a5.938 5.938 0 0 0-4.209-1.754A5.906 5.906 0 0 0 12 4.595a5.904 5.904 0 0 0-3.996-1.558 5.942 5.942 0 0 0-4.213 1.758c-2.353 2.363-2.352 6.059.002 8.412L12 21.414l8.207-8.207c2.354-2.353 2.355-6.049-.002-8.416z">
+                    </path>
+                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" class="see-comments" width="36" height="36" viewBox="0 -1.3 25 25" style="fill: rgba(0, 0, 0, 1);transform: ;msFilter:;">
+                    <path 
+                    d="M19 2H5c-1.103 0-2 .897-2 2v12c0 1.103.897 2 2 2h3.586L12 21.414 15.414 18H19c1.103 0 2-.897 2-2V4c0-1.103-.897-2-2-2zm0 14h-4.414L12 18.586 9.414 16H5V4h14v12z">
+                    </path>
+                </svg>
+                </div>
+                <div class="post-likes">
+                    <p> ${posts.data.user.edge_owner_to_timeline_media.edges[i].node.edge_media_preview_like.count} likes</p>
+                </div>
+                <div class="post-dc">
+                    <p>${userInfo.user.username} - ${posts.data.user.edge_owner_to_timeline_media.edges[i].node.edge_media_to_caption.edges[0].node.text}</p>
+                </div>
+                <div class="post-comments">
+                    <p class="postComments">View all ${posts.data.user.edge_owner_to_timeline_media.edges[i].node.edge_media_to_comment.count} comments</p>
+                </div>
             </div>
         </div>
-    </div>
-`;
+        `; 
+        
+        const postCommentElement = document.querySelectorAll('.postComments');
+
+        for (let j = 0; j < postCommentElement.length; j++) {
+            if(posts.data.user.edge_owner_to_timeline_media.edges[i].node.edge_media_to_comment.count === 0){
+                postCommentElement[i].innerHTML = "";
+                postCommentElement[i].innerHTML = "The comments have been turned off by the creator";
+            }
+        }
+
+        const noLike = document.querySelector('.no-like');
+        const likeHeart = document.querySelector('.like-heart');
+
+        noLike.addEventListener('click', () => {
+            noLike.style.display = "none";
+            likeHeart.style.display = "block";
+        })
+
+        likeHeart.addEventListener('click', () => {
+            noLike.style.display = "block";
+            likeHeart.style.display = "none";
+        })
+
+    }
+}
+
+getUserId().then(() => {getPosts()
+    .then(res => {
+        showPosts(res);
+    });
+});
+
+// ${posts[i].images.standard_resolution.url}
+// ${posts[i].likes.count}
+// ${posts[i].caption.text}
+// ${posts[i].comments.count} 
--- scripts/navBar.js
+++ scripts/navBar.js
@@ -1,184 +1,341 @@
-
-    // More Options
-    const dropUp = document.getElementById('more-menu');
-    const dropUpItems = document.getElementById('dropup-items');
-
-    dropUpItems.style.display = 'none';
-
-    dropUp.addEventListener('click', (event) => {
-        if (dropUpItems.style.display == 'none') {
-            dropUpItems.style.display = 'flex';
-        } else {
-            dropUpItems.style.display = 'none';
-        }
-    })
-
-    // window.onclick = function(event){
-    //     if(event.target == dropUpItems){
-    //         dropUpItems.style.display = 'none';
-    //     }
-    // }
-
-    // Modal for Create Post
-    const createPostModal = document.querySelector('.create-post');
-    const createPost = document.getElementById('createPostBtn');
-    const closeBtn = document.querySelector('.closeBtn')
-
-    createPost.addEventListener('click', () => {
-            createPostModal.style.display = "flex";
-    })
-    closeBtn.addEventListener('click', () => {
-            createPostModal.style.display = "none";
-    })
-    window.onclick = function (event) {
-        if (event.target == createPostModal) {
-            createPostModal.style.display = "none";
-        }
+// Change Icons
+const instaLogo1 = document.querySelector('.insta-logo1')
+const instaLogo2 = document.querySelector('.insta-logo2')
+const tabletWindow = window.matchMedia('(max-width: 1200px)');
+function changeIcon(mediaQueryList){
+    if(mediaQueryList.matches){
+        instaLogo1.style.display = "none";
+        instaLogo2.style.display = "block";
+    }else{
+        instaLogo1.style.display = "block";
+        instaLogo2.style.display = "none";
     }
+}
+tabletWindow.addListener(changeIcon);
+changeIcon(tabletWindow);
+// Nav Bar Selection
+const navBar = document.querySelector('.nav-bar');
+const navMenu = document.querySelectorAll('.menu');
 
-    // Change Icons
-    const instaLogo1 = document.querySelector('.insta-logo1')
-    const instaLogo2 = document.querySelector('.insta-logo2')
-    const tabletWindow = window.matchMedia('(max-width: 1200px)');
+// Menu Buttons
+const menuButtons = document.querySelectorAll('.menu-button');
 
-    function changeIcon(mediaQueryList){
-        if(mediaQueryList.matches){
-            instaLogo1.style.display = "none";
-            instaLogo2.style.display = "block";
+// Icons 
+var icons = document.querySelectorAll(`[id^="menu-icon"]`);
+var iconsActive = document.querySelectorAll(`[id^="menuActive"]`);
+
+// Add an event listener to each button
+menuButtons.forEach(button => {
+    button.addEventListener('click', function() {
+        // check which button is active
+        if(this.classList.contains('active')){
+            this.classList.remove('active');
+            
         }else{
-            instaLogo1.style.display = "block";
-            instaLogo2.style.display = "none";
+        // Remove the active class from all menuButtons
+        menuButtons.forEach(b => b.classList.remove('active'));
+
+        // Return to default Home Menu
+        homeIcon.style.display = "block";
+        homeIconActive.style.display = "none";   
+        homeText.style.fontWeight = "normal";
+
+        // Return to default Search Menu
+        searchIcon.style.display = "block";
+        searchIconActive.style.display = "none";   
+        searchBar.classList.remove('active');
+
+        // Return to default Explore Menu
+        exploreIcon.style.display = "block";
+        exploreIconActive.style.display = "none";   
+        exploreText.style.fontWeight = "normal";
+
+        // Return to default Notification Menu
+        notificationIcon.style.display = "block";
+        notificationIconActive.style.display = "none";   
+        notificationBar.classList.remove('active');
+
+        // Return to default Profile Menu
+        profileIcon.style.display = "block";
+        profileIconActive.style.display = "none";      
+        profileText.style.fontWeight = "normal";
+
+        // Return to defaulr More Menu
+        dropUpItems.style.display = 'none';
+        moreMenuIcon.style.display = "block";
+        moreIconActive.style.display = "none";
+        moreText.style.fontWeight = "normal";
+
+        // Return to default Navigation Bar
+        navBar.classList.remove('transition');
+        for (const element of navMenu) {
+            element.classList.remove('transition');
         }
-    }
-
-    tabletWindow.addListener(changeIcon);
-
-    changeIcon(tabletWindow);
-
-
-    // Nav Bar Selection
-    const navBar = document.querySelector('.nav-bar');
-    const navMenu = document.querySelectorAll('.menu');
-
-    // Menu Buttons
-    const menuButtons = document.querySelectorAll('.menu-button');
-
-    // Add an event listener to each button
-    menuButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            // check which button is active
-            if(this.classList.contains('active')){
-                this.classList.remove('active');
-
-            }else{
-            // Remove the active class from all menuButtons
-            menuButtons.forEach(b => b.classList.remove('active'));
-            searchBar.classList.remove('active');
-            notificationBar.classList.remove('active');
-            navBar.classList.remove('transition');
-            for (const element of navMenu) {
-                element.classList.remove('transition');
-            }
-            instaLogo1.style.display = "block";
-            instaLogo2.style.display = "none";
-
-            tabletWindow.addListener(changeIcon);
-            changeIcon(tabletWindow);
-
-            // Add the active class to the clicked button
-            this.classList.add('active');
-            console.log('active');
-            }
-        });
+        instaLogo1.style.display = "block";
+        instaLogo2.style.display = "none";  
+        tabletWindow.addListener(changeIcon);
+        changeIcon(tabletWindow);
+        // Add the active class to the clicked button
+        this.classList.add('active');
+        console.log('active');
+        }
     });
 
-    // Logo Display for Tablet Media Query
-    function tablet(media){
-        if(media.matches){
-            instaLogo1.style.display = "none";
-            instaLogo2.style.display = "block";
+});
+
+// Zoom In, Zoom Out
+for(let i = 0; i < menuButtons.length; i++){
+    // Zoom In While Hover
+    menuButtons[i].onmouseover = function(){
+        for (let j = 0; j < icons.length; j++) {
+            console.log("hover");
+            icons[i].style.transform = "scale(1.15)";
+        }
+        for (let k = 0; k < iconsActive.length; k++) {
+            console.log("hover");
+            iconsActive[i].style.transform = "scale(1.15)";
         }
     }
 
-    // Search
-    const searchButton = document.querySelector('#search-button');
-    const searchBar = document.querySelector('.search');
-
-    searchButton.addEventListener('click', function(){
-        // this.classList.toggle('active');
-
-        if(this.classList.contains('active')){
-            searchBar.classList.toggle('active');
-        }else{
-            searchBar.classList.remove('active');
+    // Zoom Out While Leaving Hover
+    menuButtons[i].onmouseleave = function(){
+        for (let j = 0; j < icons.length; j++) {
+            console.log("hover out");
+            icons[i].style.transform = "scale(1)";
         }
-
-        if (instaLogo1.style.display === "block") {
-            instaLogo1.style.display = "none";
-            instaLogo2.style.display = "block";
+        for (let k = 0; k < iconsActive.length; k++) {
+            console.log("hover out");
+            iconsActive[i].style.transform = "scale(1)";
         }
-        else {
-            instaLogo1.style.display = "block";
-            instaLogo2.style.display = "none";
-        }
+    }
+}
 
-        tabletWindow.addListener(tablet);
-        tablet(tabletWindow);
 
-        navBar.classList.toggle('transition');
-        for (const element of navMenu) {
-            element.classList.toggle('transition');
-        }
 
-    })
+// Logo Display for Tablet Media Query
+function tablet(media){
+    if(media.matches){
+        instaLogo1.style.display = "none";
+        instaLogo2.style.display = "block";  
+    }
+}
 
-    // Notification
-    const notificationButton = document.querySelector('#notification-button');
-    const notificationBar = document.querySelector('.notification');
+// Home
+const homeButton = document.querySelector('#home-button');
+const homeIcon = document.querySelector('.home-icon');
+const homeIconActive = document.querySelector('.home-icon-active');
+const homeText = document.querySelector('#home-menu');
 
-    notificationButton.addEventListener('click', function(){
-        // this.classList.toggle('active');
+homeButton.addEventListener('click', function(){
+        homeIcon.style.display = "none";
+        homeIconActive.style.display = "block";   
+        homeText.style.fontWeight = "bold";
+})
 
-        if(this.classList.contains('active')){
-            notificationBar.classList.toggle('active');
-        }else{
-            notificationBar.classList.remove('active');
-        }
 
-        if (instaLogo1.style.display === "block") {
-            instaLogo1.style.display = "none";
-            instaLogo2.style.display = "block";
-        }
-        else {
-            instaLogo1.style.display = "block";
-            instaLogo2.style.display = "none";
-        }
+// Search
+const searchButton = document.querySelector('#search-button');
+const searchBar = document.querySelector('.search');
+const searchIcon = document.querySelector('.search-icon')
+const searchIconActive = document.querySelector('.search-icon-active')
 
-        tabletWindow.addListener(tablet);
-        tablet(tabletWindow);
+searchButton.addEventListener('click', function(){
+    // this.classList.toggle('active');
 
-        navBar.classList.toggle('transition');
-        for (const element of navMenu) {
-            element.classList.toggle('transition');
-        }
-    })
+    if(this.classList.contains('active')){
+        searchBar.classList.toggle('active');
+    }else{
+        searchBar.classList.remove('active');    
+    }
+    if (instaLogo1.style.display === "block") {
+        instaLogo1.style.display = "none";
+        instaLogo2.style.display = "block";   
+    }
+    else {
+        instaLogo1.style.display = "block";
+        instaLogo2.style.display = "none";
+    }
+    tabletWindow.addListener(tablet);
+    tablet(tabletWindow);
+    navBar.classList.toggle('transition');
+    for (const element of navMenu) {
+        element.classList.toggle('transition');
+    }
 
-    // Clear Input Value
-    const searchIcon = document.querySelector('.search-icon');
-    const searchInput = document.querySelector('#search-input');
-    const clearInput = document.querySelector('.clear-input');
-
-    searchInput.addEventListener('click', () => {
-        // console.log('input is clicked');
-        if (clearInput.style.display === "none"){
-            clearInput.style.display = "block";
-            searchIcon.style.display = "none";
-        }
-    })
-
-    clearInput.addEventListener('click', () => {
-        // console.log('clear');
-        searchInput.value = '';
-        clearInput.style.display = "none";
+    if (searchIcon.style.display === "block") {
+        searchIcon.style.display = "none";
+        searchIconActive.style.display = "block";   
+    }
+    else {
         searchIcon.style.display = "block";
-    })
+        searchIconActive.style.display = "none";
+    }
+
+})
+
+// Clear Input Value
+const searchIconInput = document.querySelector('.search-icon');
+const searchInput = document.querySelector('#search-input');
+const clearInput = document.querySelector('.clear-input');
+
+searchInput.addEventListener('click', () => {
+    // console.log('input is clicked');
+    if (clearInput.style.display === "none"){
+        clearInput.style.display = "block";
+        searchIconInput.style.display = "none";
+    }
+})
+
+clearInput.addEventListener('click', () => {
+    // console.log('clear');
+    searchInput.value = '';
+    clearInput.style.display = "none";
+    searchIconInput.style.display = "block";
+})
+
+
+// Explore
+const exploreButton = document.querySelector('#explore-button');
+const exploreIcon = document.querySelector('.explore-icon');
+const exploreIconActive = document.querySelector('.explore-icon-active');
+const exploreText = document.querySelector('#explore-menu');
+
+exploreButton.addEventListener('click', function(){
+        exploreIcon.style.display = "none";
+        exploreIconActive.style.display = "block";   
+        exploreText.style.fontWeight = "bold";
+})
+
+
+// Notification
+const notificationButton = document.querySelector('#notification-button');
+const notificationBar = document.querySelector('.notification');
+const notificationIcon = document.querySelector('.notification-icon');
+const notificationIconActive = document.querySelector('.notification-icon-active');
+
+notificationButton.addEventListener('click', function(){
+    // this.classList.toggle('active');
+    if(this.classList.contains('active')){
+        notificationBar.classList.toggle('active');
+    }else{
+        notificationBar.classList.remove('active');    
+    }
+    if (instaLogo1.style.display === "block") {
+        instaLogo1.style.display = "none";
+        instaLogo2.style.display = "block";   
+    }
+    else {
+        instaLogo1.style.display = "block";
+        instaLogo2.style.display = "none";
+    }
+    tabletWindow.addListener(tablet);
+    tablet(tabletWindow);
+    navBar.classList.toggle('transition');
+    for (const element of navMenu) {
+        element.classList.toggle('transition');
+    }
+
+    if (notificationIcon.style.display === "block") {
+        notificationIcon.style.display = "none";
+        notificationIconActive.style.display = "block";   
+    }
+    else {
+        notificationIcon.style.display = "block";
+        notificationIconActive.style.display = "none";
+    }
+})
+
+
+// Modal for Create Post
+const createPostModal = document.querySelector('.create-post');
+const createPost = document.getElementById('createPostBtn');
+const closeBtn = document.querySelector('.closeBtn')
+const createIcon = document.querySelector('.create-icon');
+const createIconActive = document.querySelector('.create-icon-active');
+const createText = document.querySelector('#create-menu');
+
+createPost.addEventListener('click', () => {
+        createPostModal.style.display = "flex";
+        createIcon.style.display = "none";
+        createIconActive.style.display = "block";   
+        createText.style.fontWeight = "bold";
+})
+
+closeBtn.addEventListener('click', () => {
+        createPostModal.style.display = "none";
+        createIcon.style.display = "block";
+        createIconActive.style.display = "none";   
+        createText.style.fontWeight = "normal";
+})
+
+window.onclick = function (event) {
+    if (event.target == createPostModal) {
+        createPostModal.style.display = "none";
+        createIcon.style.display = "block";
+        createIconActive.style.display = "none";   
+        createText.style.fontWeight = "normal";
+    }
+    // if(event.target == dropUpItems){
+    //     dropUpItems.style.display = 'none';
+    // }
+}
+
+
+// Profile
+const profileButton = document.querySelector('#profile-button');
+const profileIcon = document.querySelector('.profileOne');
+const profileIconActive = document.querySelector('.profile');
+const profileText = document.querySelector('#profile-menu');
+
+profileButton.addEventListener('click', function(){
+        profileIcon.style.display = "none";
+        profileIconActive.style.display = "block";      
+        profileText.style.fontWeight = "bold";
+})
+
+
+// More Options
+const dropUp = document.getElementById('more-menu');
+const dropUpItems = document.getElementById('dropup-items');
+const moreMenuIcon = document.querySelector('.more-menu-icon');
+const moreIconActive = document.querySelector('.more-icon-active');
+const moreText = document.querySelector('#more-menu');
+
+dropUpItems.style.display = 'none';
+
+dropUp.addEventListener('click', () => {
+    if (dropUpItems.style.display == 'none') {
+        dropUpItems.style.display = 'flex';
+    } else {
+        dropUpItems.style.display = 'none';
+    }
+
+
+    if (moreMenuIcon.style.display === "block") {
+        moreMenuIcon.style.display = "none";
+        moreIconActive.style.display = "block";   
+        moreText.style.fontWeight = "bold";
+    }
+    else {
+        moreMenuIcon.style.display = "block";
+        moreIconActive.style.display = "none";
+        moreText.style.fontWeight = "normal";
+    }
+
+})
+
+// More Icon
+var moreIcon = document.getElementById("more-icon"); 
+var moreActive = document.getElementById("moreActive")
+
+// Zoom In 
+dropUp.onmouseover = function(){
+    moreIcon.style.transform = "scale(1.15)";
+    moreActive.style.transform = "scale(1.15)";
+}
+
+// Zoom Out
+dropUp.onmouseleave = function(){
+    moreIcon.style.transform = "scale(1)";
+    moreActive.style.transform = "scale(1)";
+}
