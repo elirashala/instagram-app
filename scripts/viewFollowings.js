f5a255428f65b199b6e94c2c7b6755bb3a268826
kaslara <dilara.kastrati@life.gjirafa.com>
2023-02-02 16:45:01 +0100

    get followings

--- scripts/viewFollowings.js
+++ scripts/viewFollowings.js
@@ -15,3 +15,45 @@ const viewFollowingsModal = document.querySelector('.followingsModal');
          viewFollowingsModal.style.display = "none";
      }
  }
+
+ const userfollowings = document.querySelector('.followingsModal-container');
+
+const getfollowings = async () => {
+    try {
+        const followingsRes = await fetch("https://api.npoint.io/d215d2f8d900dbb14b3f");
+        const followingsData = await followingsRes.json();
+
+        return followingsData;
+
+        } catch(error) {
+            console.error(error);
+            return {};
+
+}
+}
+
+const showfollowings = (data) => {
+    const followingsData = data.users;
+    let followingsList = "";
+
+    for (let i = 0; i < followingsData.length; i++) {
+        let profilePicUrl = followingsData[i].profile_pic_url || "";
+        let fullName = followingsData[i].full_name || "";
+        let username = followingsData[i].username || "";
+    
+        followingsList +=
+            `<div class="followings-content">
+                 <img class="followings-user-profile" src="https://source.unsplash.com/random/200x200?sig=${profilePicUrl}" alt="">
+                    <div class="followings-username">
+                        <p>${fullName}</p>
+                        <p>${username}</p>
+                     </div>
+             </div>`;
+    }
+    
+    userfollowings.innerHTML += followingsList;
+}
+
+getfollowings().then(res => {
+    showfollowings(res);
+});
